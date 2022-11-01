f6fbb639013863215d2a6c308e2da5800c40c353
efran <efran@bitcoin.co.id>
2022-11-01 13:37:37 +0700

    useState()

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -22,6 +22,10 @@
     </div>
     <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
     <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
 
@@ -46,4 +50,25 @@ const title = computed(() => `${lesson.value.title} - ${course.title}`);
 useHead({
   title,
 });
+
+const progress = useState("progress", () => {
+  return [];
+});
+
+const isLessonComplete = computed(() => {
+  if (!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
+  if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+    return false;
+  }
+  return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+});
+const toggleComplete = () => {
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
+  progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+    !isLessonComplete.value;
+};
 </script>
