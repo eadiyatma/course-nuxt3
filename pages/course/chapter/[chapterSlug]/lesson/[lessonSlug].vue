30d8d8ad4658dd2b459794b9785c9f18e0dc17dc
efran <efran@bitcoin.co.id>
2022-11-01 10:45:33 +0700

    first commit

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex flex-row space-x-4 mb-8">
+      <NuxtLink
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </NuxtLink>
+      <NuxtLink
+        v-if="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.downloadUrl"
+      >
+        Download Video
+      </NuxtLink>
+    </div>
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
+  </div>
+</template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (value) => value.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (value) => value.slug === route.params.lessonSlug
+  );
+});
+</script>
