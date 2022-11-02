8b88211bd4c0b36079e4e781f2558678bd585ffd
efran <efran@bitcoin.co.id>
2022-11-02 09:23:44 +0700

    deploying to netlify

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,12 @@
     <h1 class="text-2xl text-salmon">hello world</h1>
   </div>
 </template>
+
+<script setup>
+const router = useRouter();
+onMounted(() => {
+  router.push(
+    "/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
+  );
+});
+</script>
