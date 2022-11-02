b3ff82ee8055f93d66eb07cf489e7c09e1a8e65e
efran <efran@bitcoin.co.id>
2022-11-02 09:53:52 +0700

    add layouts

--- pages/course.vue
+++ pages/course.vue
@@ -1,44 +1,38 @@
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
-        </span>
-      </h1>
-    </div>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
+  </div>
 
-    <div class="flex flex-row justify-center flex-grow">
-      <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
-      >
-        <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
-        <div class="flex flex-col gap-2">
-          <div v-for="chapter in chapters" :key="chapter.slug">
-            <h4>
-              {{ chapter.title }}
-            </h4>
-            <NuxtLink
-              v-for="(lesson, index) in chapter.lessons"
-              :key="lesson.slug"
-              :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
-              class="flex flex-row gap-2 py-1 no-underline"
-              :class="lesson.path == $route.fullPath ? 'text-blue-500' : ''"
-            >
-              <span class="text-gray-500">{{ index + 1 }}</span>
-              <span>{{ lesson.title }}</span>
-            </NuxtLink>
-          </div>
+  <div class="flex flex-row justify-center flex-grow">
+    <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
+      <h3>Chapters</h3>
+      <!-- All the lessons for the course listed here -->
+      <div class="flex flex-col gap-2">
+        <div v-for="chapter in chapters" :key="chapter.slug">
+          <h4>
+            {{ chapter.title }}
+          </h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            class="flex flex-row gap-2 py-1 no-underline"
+            :class="lesson.path == $route.fullPath ? 'text-blue-500' : ''"
+          >
+            <span class="text-gray-500">{{ index + 1 }}</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
         </div>
       </div>
+    </div>
 
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
-        <NuxtPage />
-      </div>
+    <div class="prose p-12 bg-white rounded-md w-[65ch]">
+      <NuxtPage />
     </div>
   </div>
 </template>
